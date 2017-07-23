a32e50d7b8ba0e8d6189d37318cf0c04643bff2a
Avi Hayoun <hayounav@cs.bgu.ac.il>
2017-07-23 21:55:08 +0300

    begin work on translating Z3 linearizable counter to lean. this commit includes type definitions

--- counter.lean
+++ counter.lean
@@ -1,5 +1,23 @@
 namespace counter
 
+--------------------------------------------
+-- generic linearizability type definitions
+--------------------------------------------
+variables (E V PID : Type)
+variable α : E → E
+variable pre : E → E → bool
+variable PRE : E → E → bool
+variable in_p : E → PID → bool
+variable val : E → V
 
+--------------------------------------------
+-- counter-specific type definitions
+--------------------------------------------
+variable inc : E → bool
+variable dec : E → bool
+
+--------------------------------------------
+-- assertions
+--------------------------------------------
 
 end counter
